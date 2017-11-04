e5aa64771846d2f9d531ba0a6d22f6fa3f3ce0da
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2017-11-04 11:02:19 -0500

    Switch to non-blocking IO

--- lib/rubbis/server.rb
+++ lib/rubbis/server.rb
@@ -7,39 +7,106 @@ module Rubbis
     end
 
     def listen
-      socket = TCPServer.new(port)
+      readable = []
+      clients = {}
+      server = TCPServer.new(port)
+
+      readable << server
 
       loop do
-        Thread.start(socket.accept) do |client|
-          handle_client client
+        ready_to_read, _ = IO.select(readable + clients.keys)
+
+        ready_to_read.each do |socket|
+          case socket
+          when server
+            child_socket = socket.accept
+            clients[child_socket] = Handler.new(child_socket)
+          else
+            begin
+              clients[socket].process!
+            rescue EOFError
+              clients.delete(socket)
+              socket.close
+            end
+          end
         end
       end
     ensure
-      socket.close if socket
+      (readable + clients.keys).each do |socket|
+        socket.close
+      end
     end
 
-    def handle_client(client)
-      loop do
-        header = client.gets.to_s
+    class Handler
+      attr_reader :client, :buffer
 
-        return unless header.start_with?("*")
+      def initialize(socket)
+        @client = socket
+        @buffer = ""
+      end
 
-        num_args = header[1..-1].to_i
+      def process!
+        buffer << client.read_nonblock(1024)
 
-        cmd = num_args.times.map do
-          len = client.gets[1..-1].to_i
-          client.read(len + 2).chomp
+        cmds, processed = unmarshal(buffer)
+        @buffer = buffer[processed..-1]
+
+        cmds.each do |cmd|
+          response = case cmd.first.downcase
+                     when "ping" then "+PONG\r\n"
+                     when "echo" then "$#{cmd[1].length}\r\n#{cmd[1]}\r\n"
+                     end
+
+          client.write(response)
+        end
+      end
+
+      class ProtocolError < RuntimeError; end
+
+      def unmarshal(data)
+        io = StringIO.new(data)
+        result = []
+        processed = 0
+
+        begin
+          loop do
+            header = safe_readline(io)
+
+            raise ProtocolError unless header.start_with?("*")
+
+            n = header[1..-1].to_i
+
+            result << n.times.map do
+              raise ProtocolError unless io.readpartial(1) == "$"
+
+              length = safe_readline(io).to_i
+              safe_readpartial(io, length).tap do
+                safe_readline(io)
+              end
+            end
+
+            processed = io.pos
+          end
+        rescue ProtocolError
+          processed = io.pos
+        rescue EOFError
+          # Incomplete command, ignore
         end
 
-        response = case cmd.first.downcase
-                   when "ping" then "+PONG\r\n"
-                   when "echo" then "$#{cmd[1].length}\r\n#{cmd[1]}\r\n"
-                   end
-
-        client.write(response)
+        [result, processed]
       end
-    ensure
-      client.close
+
+      def safe_readline(io)
+        io.readline("\r\n").tap do |line|
+          raise EOFError unless line.end_with?("\r\n")
+        end
+      end
+
+      def safe_readpartial(io, length)
+        io.readpartial(length).tap do |data|
+          raise EOFError unless data.length == length
+        end
+      end
     end
 
     private
