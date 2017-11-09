da04a4af635e5a4aa677f53b9ba7c4253a7d11a9
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2017-11-09 16:52:21 -0500

    Refactor acceptance test and include helper

--- spec/acceptance/skeleton_spec.rb
+++ spec/acceptance/skeleton_spec.rb
@@ -1,8 +1,4 @@
-require "redis"
-
-require "rubbis/server"
-
-TEST_PORT = 6380
+require "spec_helper"
 
 describe Rubbis::Server, :acceptance do
   it "responds to ping" do
@@ -34,42 +30,5 @@ describe Rubbis::Server, :acceptance do
       expect(client.set("abc", "123")).to eq("OK")
       expect(client.get("abc")).to eq("123")
     end
-  end
-
-  def client
-    Redis.new(host: "localhost", port: TEST_PORT)
-  end
-
-  def with_server
-    server = nil
-    server_thread = Thread.new do
-      server = Rubbis::Server.new(TEST_PORT)
-      server.listen
-    end
-
-    wait_for_open_port TEST_PORT
-
-    yield
-  rescue Timeout::Error
-    sleep 0.01
-    server_thread.value unless server_thread.alive?
-
-    raise
-  ensure
-    server.shutdown if server
-  end
-
-  def wait_for_open_port(port)
-    time = Time.now
-    while !check_port(port) && 1 > Time.now - time
-      sleep 0.01
-    end
-
-    raise Timeout::Error unless check_port(port)
-  end
-
-  def check_port(port)
-    `nc -z localhost #{port}`
-    $?.success?
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,48 @@
+require "redis"
+
+require "rubbis/server"
+
+TEST_PORT = 6380
+
+module AcceptanceHelpers
+  def client
+    Redis.new(host: "localhost", port: TEST_PORT)
+  end
+
+  def with_server
+    server = nil
+    server_thread = Thread.new do
+      server = Rubbis::Server.new(TEST_PORT)
+      server.listen
+    end
+
+    wait_for_open_port TEST_PORT
+
+    yield
+  rescue Timeout::Error
+    sleep 0.01
+    server_thread.value unless server_thread.alive?
+
+    raise
+  ensure
+    server.shutdown if server
+  end
+
+  def wait_for_open_port(port)
+    time = Time.now
+    while !check_port(port) && 1 > Time.now - time
+      sleep 0.01
+    end
+
+    raise Timeout::Error unless check_port(port)
+  end
+
+  def check_port(port)
+    `nc -z localhost #{port}`
+    $?.success?
+  end
+end
+
+RSpec.configure do |c|
+  c.include AcceptanceHelpers, acceptance: true
+end
